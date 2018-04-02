507de6b859055dd201968ca92fa63e133c5138d0
Devon <defitjo16@gmail.com>
2018-04-02 02:41:01 -0400

    added temperature conversion

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const form = document.querySelector("form");
+const myKey = config.MY_KEY;
+
+const form = document.querySelector('form');
 
 const showWeather = (weather, temp) => {
-  document.getElementById("my_weather").innerText = `${temp}\u2109 and ${weather}`;
+  document.getElementById('my_weather').innerText = `${Math.round((temp - 273.15) * 1.8) + 32}\u2109 and ${weather}`;
 };
 
-const getWeather = (city) => {
-  city = city;
+const getWeather = (cities) => {
+  city = cities;
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}
-    &appid=00e2d6bc3e2a0a1ccf62131112e0b068`, {mode: 'cors'})
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(response) {
+    &appid=${myKey}`, { mode: 'cors' })
+    .then(response => response.json())
+    .then((response) => {
       weather = response.name;
       showWeather(response.weather[0].description, response.main.temp);
     })
-    .catch(function(error) {
-      document.getElementById("my_weather").innerText = "Error! That isn't a city -_-";
-    })
-}
+    .catch(() => {
+      document.getElementById('my_weather').innerText = "Error! That isn't a city -_-";
+    });
+};
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  getWeather(form.elements["city"].value);
+  getWeather(form.elements.city.value);
 });
